555e8de638862f91b7a1e261f855609d35b15af8
lyascat <lyascat@gmail.com>
2019-10-09 00:25:00 -0500

    Agrega funciones de encode y decode en cipher.js

---
+++ src/index.js
@@ -0,0 +1,15 @@
+btn1.addEventListener('click',()=>{
+
+  pag1.classList.add('hide');
+  pag2.classList.remove('hide');
+})
+
+btn2.addEventListener('click',()=>{
+
+
+})
+
+btn3.addEventListener('click',()=>{
+
+
+})
