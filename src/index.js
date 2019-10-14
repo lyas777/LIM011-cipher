161195e849529a522660f898d50413a6b2b3f67f
lyascat <lyascat@gmail.com>
2019-10-14 05:22:31 -0500

    Se ha actualizado el CSS

--- src/index.js
+++ src/index.js
@@ -1,29 +1,22 @@
 const btn1 = document.getElementById("btn1");
 const btn2 = document.getElementById("btn2");
-const btn3 = document.getElementById("btn2");
+const btn3 = document.getElementById("btn3");
+let texto = document.getElementById('texto');
+let clave = document.getElementById('clave');
+let resultado = document.getElementById('resultado');
 
 btn1.addEventListener('click', (event) => {
   event.preventDefault();
-  let texto = document.getElementById('texto').value;
-  let clave = document.getElementById('clave').value;
-
-  let resultado = document.getElementById('resultado');
-  resultado.value = window.cipher.encode(texto, clave);
+  resultado.value = window.cipher.encode(texto.value, clave.value);
 
 })
 btn2.addEventListener('click', (event) => {
   event.preventDefault();
-  let texto = document.getElementById('texto').value;
-  let clave = document.getElementById('clave').value;
-  let resultado = document.getElementById('resultado');
-  resultado.value = window.cipher.decode(texto, clave);
+  resultado.value = window.cipher.decode(texto.value, clave.value);
 })
 
 btn3.addEventListener('click', (event) => {
   event.preventDefault();
-  let texto = document.getElementById('texto');
-  let clave = document.getElementById('offset');
-  let resultado = document.getElementById('resultado');
   texto.value = '';
   clave.value = '';
   resultado.value = '';
